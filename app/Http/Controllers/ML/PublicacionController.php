eca085fadfe8ae4bc3f8e9fe6a72f69b5d699d41
joaquin3684 <joaquinmazoud@gmail.com>
2019-05-18 12:50:46 -0300

    lkjaslkjf

--- app/Http/Controllers/ML/PublicacionController.php
+++ app/Http/Controllers/ML/PublicacionController.php
@@ -23,7 +23,6 @@ class PublicacionController extends Controller
         $itemsSinMap = collect($items)->map(function($i){
             return $i->body;
         });
-        return $itemsSinMap;
         $publis = PublicacionMapper::map($itemsSinMap);
 
         return $publis;
