bf099f1c02c7bc6a91ccc9db9e6a3479160a63eb
joaquin3684 <joaquinmazoud@gmail.com>
2019-05-18 12:42:56 -0300

    lkjaslkjf

--- app/Http/Controllers/ML/PublicacionController.php
+++ app/Http/Controllers/ML/PublicacionController.php
@@ -19,7 +19,7 @@ class PublicacionController extends Controller
         $items = $meli->get('users/'.$user->id.'/items/search', $params)['body']->results;
 
         $params = array('access_token' => $meli->getToken(), 'ids' => implode(",",$items));
-
+        return $meli->get('items', $params);
         $publis = PublicacionMapper::map($meli->get('items', $params)['body']);
 
         return $publis;
