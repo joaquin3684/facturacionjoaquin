bdbceb483e227dd21b37bfc307cbc174cd97afe8
joaquin3684 <joaquinmazoud@gmail.com>
2019-05-27 17:29:28 -0300

    lkajsdflkj

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -53,9 +53,12 @@ class ProductoController extends Controller
 
     public function all(Request $request)
     {
-       return Producto::with(['tipo.componentes' => function($q){
+        $com =  Producto::with('tipo.componentes')->where('tipo_type', 'App\Compuesto')->where('id_empresa', $request['idEmpresa'])->get();
+        $simp =  Producto::with('tipo')->where('tipo_type', 'App\Simple')->where('id_empresa', $request['idEmpresa'])->get();
+        return $com->union($simp);
+        return Producto::with(['tipo.componentes' => function($q){
            return $q->where('productos.tipo_type', 'App\Compuesto');
-       }])->where('id_empresa', $request['idEmpresa'])->get();
+       }]);
     }
 
     public function delete($id)
